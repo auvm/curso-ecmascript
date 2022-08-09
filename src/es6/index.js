1059905b89830275a1dc191d61e11e49208cd140
auvm <angelurielvelascom@gmail.com>
2022-08-09 14:08:53 -0500

    clases y arrow functions, funciones anónimas

--- src/es6/index.js
+++ src/es6/index.js
@@ -58,7 +58,7 @@ let lorem2 = `Lorem ipsum dolor sit amet.
 // tradicional
  console.log(person.name, person.age, person.country);
 // nueva
- let{name, age, country} = person;
+ //let{name, age, country} = person;
  console.log(name, age, country);
 
 
@@ -103,3 +103,102 @@ console.log(globalVar);
 const a = "b";
 // a = "a"; produce un error, no se puede reasignar
 console.log(a);
+
+
+// --------- nueva clase
+
+// declarar y usar objetos
+let name = 'auvm';
+let age = 22;
+//tradicional --- es5
+obj = {name: name, age:age};
+
+//nueva forma --- es6
+//toma la llave y el valor de las 
+//variables
+obj2 = {name, age};
+console.log(obj2);
+
+
+// funciones de tipo flecha arrow
+
+const names = [
+    {name: 'auvm', age:22},
+    {name: 'Miguel', age:23}
+];
+
+// tradicional
+//función anónima que imprime todos los nombres
+                //map a c/u
+                        // función anónima con  
+                            //item de parámetro
+const listOfNames = names.map(function(item){
+    console.log(item.name);
+});
+
+//nuevas arrow function
+                //función item de parámetro 
+                        //y lo imprime
+const listOfNames2 = names.map(item => console.log(item.name));
+
+//si necesitamos más de un parámetro
+const listOfNames3 = (name, age, county) => {
+    // ...
+}
+
+//si solo es uno y necesita un bloque de código
+const listOfNames4 = name => {
+    // ...
+}
+
+//solo un parámetro y su retorno sencillo
+const squere = num => num * num;
+
+
+// promesas para asincronismo
+
+const helloPromise = () =>{
+    return new Promise((resolve, reject) => {
+        if(false){
+            resolve("Hey!");
+        }else{
+            reject("Ups...");
+        }
+    });
+}
+
+helloPromise()
+    .then(Response => console.log(Response) )
+    .then(() => console.log("Hola OOC"))
+    .catch(Error => console.log(Error));
+    
+
+
+
+
+// clases en javaScript
+class Calculator{
+    constructor(){
+        //declara las variables a usar
+        //en la clase
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+
+    sum(valueA, valueB){
+        //a las variables
+                    //asigna los argumentos
+                    //recibidos
+        this.valueA = valueA;
+        this.valueB = valueB;
+
+        //retorna sus valores
+        return this.valueA + this.valueB;
+    }
+}
+
+//declara e instancía un elemento de la clase
+const calc = new Calculator();
+//invoca al metodo de la instancia y le pasa
+//los dos argumentos
+console.log(calc.sum(2, 2));
